7b1e2cf56369009acb86646a99c5e6f7a9f05817
kag2219 <kag2219@columbia.edu>
2022-07-07 15:18:45 -0700

    finish

--- assignment1.R
+++ assignment1.R
@@ -2,22 +2,21 @@
 
 
 # Create a variable 'x' that holds a value of 9
-
+x <- 9
 # Create a variable 'y' that holds a value of x
-
+y <- x
 # Create a variable 'z' that holds the sum of x and y
-
+z <- x+y
 # Clear the variable x from the workspace
-
+rm(x)
 # Clear all variables from the workspace
-
+rm(list=ls())
 # Make a statement that directs to http://www.google.com
-
+browseURL("http://www.google.com")
 # Show a list of all packages installed
-
+library()
 # Show a list of packages currently installed and usable
-
-
+search()
 
 # Magic Number Exercise
 # Do the Following:
@@ -26,3 +25,10 @@
     # Subtract 6 from the answer.
     # Divide what you get by 3.
 # you should end up with the same number that you started with
+num <-10
+#num <- num+2
+#num <- num*3
+#num <- num-6
+#num <- num/3
+#num
+num <- ((10+2)*3-6)/3
